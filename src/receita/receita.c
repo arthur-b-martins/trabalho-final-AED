1bc2ce4fb3f4a9e212d702f6d3a585b1ef7213db
arthur-b-martins <arthurborgesmartins4@gmail.com>
2025-04-16 16:36:07 -0300

    function insere implemented

--- src/receita/receita.c
+++ src/receita/receita.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 Receita criaReceita(char nome[50]){
-    Receita receita = malloc(sizeof(descritorReceita));
+    Receita receita = malloc(sizeof(struct descritorReceita));
     if(!receita){
         pritnf("Erro na alocação de memória");
         exit;
@@ -12,8 +12,31 @@ Receita criaReceita(char nome[50]){
 
     strcpy(receita->nomeReceita, nome);
     receita->inicio = NULL;
-    receita->final = NULL;
     receita->quantidadeIngredientes = 0;
 
     return receita;
+}
+
+int insereIngrediente(Ingrediente ingredienteRecebido, Receita receita){
+    if(!receita){
+        return -1;        
+    }
+
+    NoIngrediente no = malloc(sizeof(struct noIngrediente));
+    no->ingrediente = ingredienteRecebido;
+
+    if(receita->quantidadeIngredientes = 0){
+        receita->inicio = no;
+        no->anterior = NULL;
+        no->proximo = NULL;
+        receita->quantidadeIngredientes++;
+    }
+
+    else {
+        receita->inicio->anterior = no;
+        no->proximo = receita->inicio;
+        receita->inicio = no;
+        receita->quantidadeIngredientes++;
+    }
+    
 }
--- src/receita/receita.h
+++ src/receita/receita.h
@@ -11,21 +11,29 @@ struct ingrediente{
     char nomeIngrediente[50];
     int quantidade;
     char medida[50];
-    bool essencial;
-    struct ingrediente *proximo;
-    struct ingrediente *anterior;
+    int essencial;
 };
 
-typedef struct ingrediente* Ingrediente;
+typedef struct ingrediente Ingrediente;
+
+struct noIngrediente{
+    Ingrediente ingrediente;
+    struct noIngrediente *proximo;
+    struct noIngrediente *anterior;
+};
+
+typedef struct noIngrediente* NoIngrediente;
 
 // no descritor da receita
 struct descritorReceita{
     char nomeReceita[50];
-    Ingrediente inicio;
-    Ingrediente final;
+    NoIngrediente inicio;
     int quantidadeIngredientes; 
 };
 
 typedef struct descritorReceita* Receita;
+
+Receita criaReceita(char nome[50]);
+void inserirIngrediente(Ingrediente ingrediente, Receita receita);
 
 #endif
